cbb0d77650c0aa50c569b0542957911fcf359cf8
Shreyas <121279572+Shreyasssonar@users.noreply.github.com>
2024-01-15 11:15:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function updateBackground() {
+            const imageUrlPortrait = 'https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg';
+            const imageUrlLandscape = 'https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg';
+
+            const body = document.body;
+            const isPortrait = window.innerWidth < window.innerHeight;
+
+            body.style.backgroundImage = `url(${isPortrait ? imageUrlPortrait : imageUrlLandscape})`;
+        }
+
+        // Initial background update on page load
+        window.addEventListener('load', updateBackground);
+
+        // Update background on window resize (orientation change)
+        window.addEventListener('resize', updateBackground);
